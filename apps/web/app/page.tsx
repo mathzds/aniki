ff03f84a3d16472ee740e6973903fea15b50068f
mathzds <pedrofilhodev@gmail.com>
2025-04-01 10:38:23 -0300

    docs: fixing packages on MD

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
 	return (
-		<div className="flex flex-row justify-center items-center h-screen">
-			Ola mundo
+		<div className="flex flex-col justify-center items-center h-screen ">
+			<Image
+				src="/logo.png"
+				alt="Application Logo"
+				width={300}
+				height={300}
+				priority
+			/>
+			<button type="button" className="btn btn-soft btn-accent transition">
+				Log-in
+			</button>
 		</div>
 	);
 }
